dfcb3bd64511c4ac4c3cf688bc3158594a9f702c
zelan <zelan.xilin@outlook.com>
2026-01-04 21:41:59 +0800

    添加 Button 和 NavigationMenu 组件，整合相关样式配置；优化组件结构以提升可复用性和可读性

---
+++ src/components/ui/navigation-menu/index.ts
@@ -0,0 +1,12 @@
+export {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from './navigation-menu';
+
+export { navigationMenuTriggerStyle } from './config';
--- src/components/ui/navigation-menu/navigation-menu.tsx
+++ src/components/ui/navigation-menu/navigation-menu.tsx
@@ -1,9 +1,9 @@
 import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
-import { cva } from 'class-variance-authority';
 import { ChevronDownIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
+import { navigationMenuTriggerStyle } from './config';
 
 function NavigationMenu({
   className,
@@ -54,10 +54,6 @@ function NavigationMenuItem({
     />
   );
 }
-
-const navigationMenuTriggerStyle = cva(
-  'group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:hover:bg-accent data-[state=open]:text-accent-foreground data-[state=open]:focus:bg-accent data-[state=open]:bg-accent/50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1',
-);
 
 function NavigationMenuTrigger({
   className,
@@ -156,6 +152,5 @@ export {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
   NavigationMenuViewport,
 };

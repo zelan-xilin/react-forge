73bb5738bf9409cb789f6010701c15c85156f131
zelan <zelan.xilin@outlook.com>
2026-01-06 11:54:09 +0800

    重构 AppTab 组件，添加关闭标签功能，优化代码结构，使用 ScrollArea 和 ScrollBar 组件

--- src/layouts/AppTab.tsx
+++ src/layouts/AppTab.tsx
@@ -1,14 +1,17 @@
-import { useMemo, useSyncExternalStore } from 'react';
+import { useMemo, useSyncExternalStore, type MouseEvent } from 'react';
 
 import { Button } from '@/components/ui/button';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { isExactPathMatch, permissionRoutes } from '@/router';
+import { X } from 'lucide-react';
+import { Link, useLocation } from 'react-router';
 import { useKeepAlive } from './keep-alive';
 
 const AppTab = () => {
-  const { getSnapshot, subscribeTabs, getTabVersion } = useKeepAlive();
+  const location = useLocation();
+  const { getSnapshot, subscribeTabs, getTabVersion, removeOutlet } = useKeepAlive();
 
   const tabVersion = useSyncExternalStore(subscribeTabs, getTabVersion, getTabVersion);
-
   const entries = useMemo(() => {
     void tabVersion;
 
@@ -25,16 +28,46 @@ const AppTab = () => {
       .filter(e => e.title);
   }, [getSnapshot, tabVersion]);
 
+  const onCloseTab = (e: MouseEvent, path: string) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (location.pathname === path) {
+      return;
+    }
+
+    removeOutlet(path, true);
+  };
+
   return (
-    <div className="flex gap-6 flex-none">
-      {entries.map(t => (
-        <Button key={t.path} className="p-2">
-          <strong>{t.title}</strong>
-          <span>:</span>
-          <span>{t.path}</span>
-        </Button>
-      ))}
-    </div>
+    <ScrollArea className="w-full whitespace-nowrap flex-none border-b">
+      <div className="flex w-max gap-2 px-4 py-0">
+        {entries.map(t => {
+          const isActive = location.pathname === t.path;
+          const baseClassName =
+            'flex gap-1 items-center rounded-sm py-1.5 px-2 my-1 text-xs hover:bg-primary/10 bg-secondary text-secondary-foreground';
+          const activeClassName = isActive ? 'bg-primary! text-primary-foreground!' : '';
+
+          return (
+            <Link key={t.path} to={t.path} className={`${baseClassName} ${activeClassName}`}>
+              <span>{t.title}</span>
+
+              {!isActive && (
+                <Button
+                  variant="ghost"
+                  className="size-3 text-destructive! hover:bg-destructive/30 rounded-full p-2!"
+                  onClick={evt => onCloseTab(evt, t.path)}
+                >
+                  <X className="size-3" />
+                </Button>
+              )}
+            </Link>
+          );
+        })}
+      </div>
+
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 };
 

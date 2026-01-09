4a2aa7515cd869bcc0d4327a925b83c46cda7273
zelan <zelan.xilin@outlook.com>
2026-01-10 00:50:22 +0800

    优化 useKeepAliveRefresh 组件，新增 refresh 方法以支持页面刷新功能

--- src/layouts/keep-alive/useKeepAliveRefresh.ts
+++ src/layouts/keep-alive/useKeepAliveRefresh.ts
@@ -3,10 +3,13 @@ import { useSyncExternalStore } from 'react';
 import { useKeepAlive } from './useKeepAlive';
 
 export const useKeepAliveRefresh = (pagePath: string) => {
-  const { getOutlet, subscribeOutlets, getOutletVersion } = useKeepAlive();
+  const { getOutlet, subscribeOutlets, getOutletVersion, refreshOutlet } = useKeepAlive();
 
   const v = useSyncExternalStore(subscribeOutlets, getOutletVersion, getOutletVersion);
   void v;
 
-  return getOutlet(pagePath)?.refreshId;
+  return {
+    refreshId: getOutlet(pagePath)?.refreshId,
+    refresh: () => refreshOutlet(pagePath),
+  };
 };

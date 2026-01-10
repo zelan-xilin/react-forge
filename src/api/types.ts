c229d85c9adf9a4f5892961316f3d02602a49d52
zelan <zelan.xilin@outlook.com>
2026-01-10 16:59:46 +0800

    新增 RequestPageDto 类型以支持分页请求的数据结构

--- src/api/types.ts
+++ src/api/types.ts
@@ -14,3 +14,11 @@ export type RequestDto<T> = Promise<{
   data: T;
   message: string;
 }>;
+
+export type RequestPageDto<T> = Promise<{
+  data: {
+    records: T;
+    total: number;
+  };
+  message: string;
+}>;
